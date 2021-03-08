fe69c2bb426f7cbd0dde59d8e13db715a4e6e716
Nguyễn Thiên <4708693-thien.nguyen2@users.noreply.gitlab.com>
2021-03-08 23:37:59 +0700

    crud project tasks

---
+++ database/seeders/TasksTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use Illuminate\Database\Seeder;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $task = new Task();
+        $task->id = 1;
+        $task->title = "Công việc 1";
+        $task->content = "Nội dung công việc 1";
+        $task->image = "";
+        $task->due_date = "2021-03-08";
+        $task->save();
+
+        $task = new Task();
+        $task->id = 2;
+        $task->title = "Công việc 2";
+        $task->content = "Nội dung công việc 2";
+        $task->image = "";
+        $task->due_date = "2021-03-08";
+        $task->save();
+
+        $task = new Task();
+        $task->id = 3;
+        $task->title = "Công việc 3";
+        $task->content = "Nội dung công việc 3";
+        $task->image = "";
+        $task->due_date = "2021-03-08";
+        $task->save();
+
+        $task = new Task();
+        $task->id = 4;
+        $task->title = "Công việc 4";
+        $task->content = "Nội dung công việc 4";
+        $task->image = "";
+        $task->due_date = "2021-03-08";
+        $task->save();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::group(['prefix' => 'tasks'], function () {
+    Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/create', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::post('/{id}/edit', [TaskController::class, 'update'])->name('tasks.update');
+    Route::get('/{id}/destroy', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
